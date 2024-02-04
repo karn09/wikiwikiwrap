09e1efd41b693774f52e057609a4ff1f98104791
John Nieves <karn09@gmail.com>
2024-02-03 23:59:28 -0500

    Update limiter.py to include storage_uri configuration

--- wikiwikiwrap/utils/limiter.py
+++ wikiwikiwrap/utils/limiter.py
@@ -6,14 +6,20 @@ def create_limiter(app, limiter_value):
     """Create and configure a rate limiter."""
 
     if limiter_value is None:
-        raise ValueError('Rate limiter value is not set.')
+        raise ValueError("Rate limiter value is not set.")
 
     if app is None:
-        raise ValueError('Flask app is not set.')
+        raise ValueError("Flask app is not set.")
+
+    storage_uri = "memory://"
+
+    if "LIMITER_STORAGE" in app.config:
+        storage_uri = app.config["LIMITER_STORAGE"]
 
     limiter = Limiter(
-        app = app,
+        app=app,
         key_func=get_remote_address,
-        default_limits=[limiter_value]
+        default_limits=[limiter_value],
+        storage_uri=storage_uri,
     )
     return limiter
