80a02ea9185ad46f446dd08573550c895ee4adb2
John Nieves <karn09@gmail.com>
2024-02-03 23:45:58 -0500

    Add rate limiter checks

--- wikiwikiwrap/utils/limiter.py
+++ wikiwikiwrap/utils/limiter.py
@@ -3,6 +3,14 @@ from flask_limiter.util import get_remote_address
 
 # Rate limiting for the Wikipedia API
 def create_limiter(app, limiter_value):
+    """Create and configure a rate limiter."""
+
+    if limiter_value is None:
+        raise ValueError('Rate limiter value is not set.')
+
+    if app is None:
+        raise ValueError('Flask app is not set.')
+
     limiter = Limiter(
         app = app,
         key_func=get_remote_address,
