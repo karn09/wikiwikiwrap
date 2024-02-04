2e4b7971cc1e759b4922dab0f5aee4adf784f701
John Nieves <karn09@gmail.com>
2024-02-03 23:26:51 -0500

    Update logging and rate limiter configurations

--- wikiwikiwrap/config.py
+++ wikiwikiwrap/config.py
@@ -18,12 +18,16 @@ class DevelopmentConfig(Config):
 
     # Development-specific logging configuration
     LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    LOGGING_LOCATION = 'development.log'
+    LOGGING_LOCATION = 'stdout'
     LOGGING_LEVEL = logging.DEBUG
-    LIMITER_VALUE = '5 per minute'
+    LIMITER_VALUE = '200 per minute'
 
 class TestingConfig(Config):
     TESTING = True
+    LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    LIMITER_VALUE = '200 per minute'
+    LOGGING_LOCATION = 'testing.log'
+    LOGGING_LEVEL = logging.DEBUG
 
 
 config_dict = {
@@ -33,7 +37,7 @@ config_dict = {
 }
 
 # Set the configuration to the value of the 'FLASK_ENV' environment variable
-# Default to 'Production' if 'FLASK_ENV' is not set
+# Default to 'development' if 'FLASK_ENV' is not set
 flask_env = os.getenv('FLASK_ENV', 'development').lower()
 config = config_dict.get(flask_env)
 
