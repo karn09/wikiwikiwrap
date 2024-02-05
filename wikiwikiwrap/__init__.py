c0d804bc3c1ebaa73bd59324fcb116297ce889ba
John Nieves <karn09@gmail.com>
2024-02-05 10:35:27 -0500

    Add cache initialization and logging for cache type and location

--- wikiwikiwrap/__init__.py
+++ wikiwikiwrap/__init__.py
@@ -5,16 +5,17 @@
 # up the configuration, logging, and other important settings.
 
 from flask import Flask
-from .api.v1 import wikipedia
 from .utils.limiter import create_limiter
 from .utils.logger import create_logger
 from .config import config
+from .utils.cache import init_cache
 
 __version__ = "0.1.0"
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
+
     app = Flask(__name__)
     app.config.from_object(config)
 
@@ -25,12 +26,22 @@ def create_app():
         app.config["LOGGING_FORMAT"],
     )
 
+    # Initialize the cache
+    if "TESTING" not in app.config or not app.config["TESTING"]:
+        init_cache(app).init_app(app)
+        app.logger.info(
+            f"Cache initialized with type: {app.config['CACHE_TYPE']} and location: {app.config['CACHE_REDIS_HOST']}:{app.config['CACHE_REDIS_PORT']}"
+        )
+
     # Initialize the rate limiter
     if "TESTING" not in app.config or not app.config["TESTING"]:
         create_limiter(app)
         app.logger.info(
             f"Global Rate limiter initialized with value: {app.config['RATELIMIT_DEFAULT']}"
         )
+
+    # Import and register the Wikipedia API blueprint
+    from .api.v1 import wikipedia
 
     # Register the Wikipedia API blueprint
     app.register_blueprint(wikipedia.bp)
